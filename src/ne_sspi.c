83d50ec353453d705d7bf7ae12a32395ce4d56b8
Joe Orton <notroj@users.noreply.github.com>
2006-01-09 14:39:05 +0000

    Revert r832 per request from Stefan Küng.

--- src/ne_sspi.c
+++ src/ne_sspi.c
@@ -468,7 +468,6 @@ int ne_sspi_authenticate(void *context, const char *base64Token, char **response
         SecBuffer inBuffer;
 
         if (!sspiContext->continueNeeded) {
-            freeBuffer(&outBufferDesc);
             NE_DEBUG(NE_DBG_HTTPAUTH, "sspi: Got an unexpected token.\n");
             return -1;
         }
@@ -477,7 +476,6 @@ int ne_sspi_authenticate(void *context, const char *base64Token, char **response
 
         status = base64ToBuffer(base64Token, &inBufferDesc);
         if (status) {
-            freeBuffer(&outBufferDesc);
             return status;
         }
 
@@ -490,7 +488,6 @@ int ne_sspi_authenticate(void *context, const char *base64Token, char **response
         freeBuffer(&inBufferDesc);
     } else {
         if (sspiContext->continueNeeded) {
-            freeBuffer(&outBufferDesc);
             NE_DEBUG(NE_DBG_HTTPAUTH, "sspi: Expected a token from server.\n");
             return -1;
         }
@@ -500,7 +497,6 @@ int ne_sspi_authenticate(void *context, const char *base64Token, char **response
 
         if (acquireCredentialsHandle
             (&sspiContext->credentials, sspiContext->mechanism) != SEC_E_OK) {
-            freeBuffer(&outBufferDesc);
             NE_DEBUG(NE_DBG_HTTPAUTH,
                      "sspi: acquireCredentialsHandle failed.\n");
             return -1;
