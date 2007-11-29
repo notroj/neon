f95eb43bd67fd362ddcfa128adff350c7f0adcb9
joe <joe@61a7d7f5-40b7-0310-9c16-bb0ea8cb1845>
2007-11-29 23:05:18 +0000

    * src/ne_openssl.c (ne_ssl_context_create): Enable client session   cache if creating server context.

--- neon/trunk/src/ne_openssl.c
+++ neon/trunk/src/ne_openssl.c
@@ -548,8 +548,10 @@ ne_ssl_context *ne_ssl_context_create(int mode)
         SSL_CTX_set_options(ctx->ctx, SSL_OP_ALL);
     } else if (mode == NE_SSL_CTX_SERVER) {
         ctx->ctx = SSL_CTX_new(SSLv23_server_method());
+        SSL_CTX_set_session_cache_mode(ctx->ctx, SSL_SESS_CACHE_CLIENT);
     } else {
         ctx->ctx = SSL_CTX_new(SSLv2_server_method());
+        SSL_CTX_set_session_cache_mode(ctx->ctx, SSL_SESS_CACHE_CLIENT);
     }
     return ctx;
 }
