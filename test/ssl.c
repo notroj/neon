e561734ec25bdc425faaed055dddb305519511d1
Joe Orton <notroj@users.noreply.github.com>
2008-07-25 13:39:21 +0000

    Merge r1505, r1512 from trunk:
    Fail with a useful error message in the case where a client cert is
    requested during handshake, none can be provided, and the handshake
    fails:
    
    * src/ne_private.h (struct ne_session_s): Add ssl_cc_requested field.
    
    * src/ne_openssl.c (provide_client_cert): Set ssl_cc_requested if
      no cert is provided.
      (ne__negotiate_ssl): Clear ssl_cc_requested before handshake.
      Use different, more useful error message if handshake fails and flag
      is now set.
    
    * test/ssl.c (struct ssl_server_args): Add fail_silently flag.
      (ssl_server): Exit with success if handshake fails and above flag set.
      (no_client_cert): New test case.
    
    
    * src/ne_gnutls.c (provide_client_cert): Set ssl_cc_requested flag
      if no client cert is provided.
      (ne__negotiate_ssl): Give useful error message if handshake fails
      and ssl_cc_requested flag is set.

--- test/ssl.c
+++ test/ssl.c
@@ -73,6 +73,7 @@ struct ssl_server_args {
     int require_cc; /* require a client cert if non-NULL */
     const char *ca_list; /* file of CA certs to verify client cert against */
     const char *send_ca; /* file of CA certs to send in client cert request */
+    int fail_silently; /* exit with success if handshake fails */
     
     /* session caching: */
     int cache; /* use the session cache if non-zero */
@@ -115,8 +116,11 @@ static int ssl_server(ne_socket *sock, void *userdata)
     ne_ssl_context_set_verify(ctx, args->require_cc, args->send_ca,
                               args->ca_list);
 
-    ONV(ne_sock_accept_ssl(sock, ctx),
-        ("SSL accept failed: %s", ne_sock_error(sock)));
+    ret = ne_sock_accept_ssl(sock, ctx);
+    if (ret && args->fail_silently) {
+        return 0;
+    }
+    ONV(ret, ("SSL accept failed: %s", ne_sock_error(sock)));
 
     args->count++;
 
@@ -971,6 +975,38 @@ static int ccert_unencrypted(void)
     return OK;
 }
 
+#define NOCERT_MESSAGE "client certificate was requested"
+
+/* Tests for useful error message if a handshake fails where a client
+ * cert was requested. */
+static int no_client_cert(void)
+{
+    ne_session *sess = DEFSESS;
+    struct ssl_server_args args = {SERVER_CERT, NULL};
+    int ret;
+
+    args.require_cc = 1;
+    args.fail_silently = 1;
+
+    ne_ssl_trust_cert(sess, def_ca_cert);
+
+    CALL(spawn_server(7777, ssl_server, &args));
+    
+    ret = any_request(sess, "/failme");
+
+    ONV(ret != NE_ERROR,
+        ("unexpected result %d: %s", ret, ne_get_error(sess)));
+
+    ONV(strstr(ne_get_error(sess), NOCERT_MESSAGE) == NULL,
+        ("error message was '%s', missing '%s'", 
+         ne_get_error(sess), NOCERT_MESSAGE));
+    
+    reap_server();
+
+    ne_session_destroy(sess);    
+    return OK;
+}
+
 /* non-zero if a server auth header was received */
 static int got_server_auth; 
 
@@ -1617,6 +1653,7 @@ ne_test tests[] = {
     T(ccert_unencrypted),
     T(client_cert_provided),
     T(cc_provided_dnames),
+    T(no_client_cert),
 
     T(parse_cert),
     T(parse_chain),
