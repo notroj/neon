f39f9ab852ed9b20304c4148a28a1709213f842d
joe <joe@61a7d7f5-40b7-0310-9c16-bb0ea8cb1845>
2010-01-08 11:09:36 +0000

    * src/ne_defs.h: Only define NE_PRIVATE if undefined.
    * configure.in: Define NE_PRIVATE to 'extern' for a static build.

--- neon/trunk/src/ne_defs.h
+++ neon/trunk/src/ne_defs.h
@@ -1,6 +1,6 @@
 /* 
    Standard definitions for neon headers
-   Copyright (C) 2003-2008, Joe Orton <joe@manyfish.co.uk>
+   Copyright (C) 2003-2008, 2010, Joe Orton <joe@manyfish.co.uk>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -51,7 +51,9 @@ typedef off_t ne_off_t;
 
 #ifdef __GNUC__
 #if __GNUC__ >= 3
+#ifndef NE_PRIVATE
 #define NE_PRIVATE __attribute__((visibility ("hidden")))
+#endif
 #define ne_attribute_malloc __attribute__((malloc))
 #else
 #define ne_attribute_malloc
