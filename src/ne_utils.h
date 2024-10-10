beed995f3f236181f390f8c0d44104d7f3ea5b93
Joe Orton <joe@manyfish.uk>
2024-10-10 16:48:43 +0100

    * src/ne_utils.h: Update doc note for NE_FEATURE_LIBPXY.

--- src/ne_utils.h
+++ src/ne_utils.h
@@ -54,7 +54,7 @@ int ne_version_match(int major, int minor);
 #define NE_FEATURE_I18N (7) /* i18n error message support */
 #define NE_FEATURE_SSPI (8) /* NTLM/Negotiate authentication protocol via SSPI */
 #define NE_FEATURE_GSSAPI (9) /* GSSAPI support. */
-#define NE_FEATURE_LIBPXY (10) /* libproxy support. */
+#define NE_FEATURE_LIBPXY (10) /* System proxy support via libproxy. */
 
 /* Returns non-zero if library is built with support for the given
  * NE_FEATURE_* feature code 'code'. */
