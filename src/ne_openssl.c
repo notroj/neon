1f1e5ed9f8f1871b6d300610a1d8e5b3c7723c7b
Joe Orton <joe@manyfish.uk>
2025-07-15 18:08:36 +0100

    Update copyright years.

--- src/ne_openssl.c
+++ src/ne_openssl.c
@@ -1,6 +1,6 @@
 /* 
    neon SSL/TLS support using OpenSSL
-   Copyright (C) 2002-2021, Joe Orton <joe@manyfish.co.uk>
+   Copyright (C) 2002-2025, Joe Orton <joe@manyfish.co.uk>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
